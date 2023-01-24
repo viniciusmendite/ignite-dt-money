fe8846ac1e2bc912f7d8e7b0d5a8334e8c913943
Vinicius Mendite <vinicius@sharbe.com.br>
2023-01-23 21:41:16 -0300

    feat: transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/12/2020</td>
+            </tr>
+            <tr>
+              <td width="50%">Food</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 2.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/12/2020</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
