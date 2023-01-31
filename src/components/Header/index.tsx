941f78c31630ea5c584e30769f787f66ebd1ee4b
Vinicius Mendite <vinicius@sharbe.com.br>
2023-01-31 17:55:29 -0300

    feat: header component, transaction modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../../assets/logo.svg';
+
+import { NewTransactionModal } from '../NewTransactionModal';
+
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 
 export function Header() {
   return (
@@ -8,7 +12,13 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
