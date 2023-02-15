1445819b6f0edeec49ab4639b0f4dcc94359cc56
Vinicius Mendite <vinicius@sharbe.com.br>
2023-02-15 18:22:13 -0300

    perf: use memo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -65,7 +65,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   useEffect(() => {
     fetchTransactions();
-  }, []);
+  }, [fetchTransactions]);
 
   return (
     <TransactionsContext.Provider
